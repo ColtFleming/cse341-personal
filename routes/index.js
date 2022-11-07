88cffc4fafadb8f51bbe22459a79ca29d70073c9
Colt Fleming <coltfleming@gmail.com>
2022-11-06 19:00:54 -0700

    Week 8 updates

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const authorizationRoutes = require('./authorization');
+//const loadUser = require('../middleware/loadUser');
 
+//router.use([loadUser]);
 router.use('/', require('./swagger'));
 router.use('/rosters', require('./rosters'));
 router.use('/schedules', require('./schedule'));
+router.use('/authorization', authorizationRoutes);
 
 module.exports = router;
